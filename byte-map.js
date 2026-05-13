fc97dab3d4e98ed36c094ec3949db25192915c35
ZyonixGaming <zyonixgaming@gmail.com>
2026-05-13 11:49:08 +0800

    Add files via upload

---
+++ byte-map.js
@@ -0,0 +1,962 @@
+BYTE_MAP={
+  "0": {
+    "name": "SIZE",
+    "n": "ATGC"
+  },
+  "1": {
+    "name": "ASPECT",
+    "n": "GCAT"
+  },
+  "2": {
+    "name": "SKINNY",
+    "n": "GACT"
+  },
+  "3": {
+    "name": "BONES",
+    "n": "GCAT"
+  },
+  "4": {
+    "name": "BONES2",
+    "n": "TGCA"
+  },
+  "5": {
+    "name": "CHEST_BIG",
+    "n": "ATCG"
+  },
+  "6": {
+    "name": "CHEST_SMALL",
+    "n": "GCTA"
+  },
+  "7": {
+    "name": "GIANT_DWARF",
+    "n": "GACT"
+  },
+  "8": {
+    "name": "MUSCLE_USE",
+    "n": "CTAG"
+  },
+  "9": {
+    "name": "QUADRUPED",
+    "n": "CGAT"
+  },
+  "10": {
+    "name": "BIPED",
+    "n": "TAGC"
+  },
+  "11": {
+    "name": "SPLAY",
+    "n": "ACTG"
+  },
+  "12": {
+    "name": "LEG_IN",
+    "n": "CGAT"
+  },
+  "13": {
+    "name": "LEG_IN2",
+    "n": "CGAT"
+  },
+  "14": {
+    "name": "GUT",
+    "n": "TGAC"
+  },
+  "15": {
+    "name": "GUT_IS_UDDER",
+    "n": "ACGT"
+  },
+  "16": {
+    "name": "OSTODERM",
+    "n": "GATC"
+  },
+  "17": {
+    "name": "OSTO_SIZE",
+    "n": "TACG"
+  },
+  "18": {
+    "name": "DERRIERE",
+    "n": "CTGA"
+  },
+  "19": {
+    "name": "SPEED_FACTOR",
+    "n": "GCAT"
+  },
+  "20": {
+    "name": "BREAK_FORCE",
+    "n": "GATC"
+  },
+  "21": {
+    "name": "TAIL_TAG",
+    "n": "TAGC"
+  },
+  "22": {
+    "name": "TAIL_EXISTS",
+    "n": "GCAT"
+  },
+  "23": {
+    "name": "TAIL_SIZE",
+    "n": "GCAT"
+  },
+  "24": {
+    "name": "TAIL_SHORT",
+    "n": "CAGT"
+  },
+  "25": {
+    "name": "TAIL_ASPECT",
+    "n": "AGCT"
+  },
+  "26": {
+    "name": "TAIL_ANGLE",
+    "n": "GCAT"
+  },
+  "27": {
+    "name": "TAIL_JOINT_TYPE",
+    "n": "ATGC"
+  },
+  "28": {
+    "name": "TAIL_STIFF",
+    "n": "ATGC"
+  },
+  "29": {
+    "name": "TAIL_SPEED",
+    "n": "GCAT"
+  },
+  "30": {
+    "name": "TAIL_FLEXIBILITY",
+    "n": "GTAC"
+  },
+  "31": {
+    "name": "TAIL_SHAPE",
+    "n": "AGTC"
+  },
+  "32": {
+    "name": "TAIL_BOTTOM",
+    "n": "GTCA"
+  },
+  "33": {
+    "name": "TAIL_SEGMENTS",
+    "n": "GTCA"
+  },
+  "34": {
+    "name": "LEG_TAG",
+    "n": "ATGC"
+  },
+  "35": {
+    "name": "LEG_TYPE",
+    "n": "TACG"
+  },
+  "36": {
+    "name": "LEG_LENGTH",
+    "n": "CATG"
+  },
+  "37": {
+    "name": "LEG_STRETCH",
+    "n": "GACT"
+  },
+  "38": {
+    "name": "LEG_STRETCH2",
+    "n": "ATCG"
+  },
+  "39": {
+    "name": "LEG_STRENGTH",
+    "n": "GCTA"
+  },
+  "40": {
+    "name": "LEG_HAS_FOOT",
+    "n": "GACT"
+  },
+  "41": {
+    "name": "LEG_JOINT_TYPE",
+    "n": "AGTC"
+  },
+  "42": {
+    "name": "LEG_FLEXIBILITY",
+    "n": "GTCA"
+  },
+  "43": {
+    "name": "LEG_FLEX_BIAS",
+    "n": "CGAT"
+  },
+  "44": {
+    "name": "LEG_THRUST_BACK",
+    "n": "GCTA"
+  },
+  "45": {
+    "name": "LEG_IS_CIRCLE",
+    "n": "ATGC"
+  },
+  "46": {
+    "name": "LEG_COUNT",
+    "n": "CGTA"
+  },
+  "47": {
+    "name": "LEG_SKEW",
+    "n": "TCGA"
+  },
+  "48": {
+    "name": "LEG_PENCIL",
+    "n": "ATGC"
+  },
+  "49": {
+    "name": "LEG_AND_ARM_LIMP",
+    "n": "TAGC"
+  },
+  "50": {
+    "name": "HAS_KNEE",
+    "n": "TCGA"
+  },
+  "51": {
+    "name": "KNEE_MIN",
+    "n": "CGAT"
+  },
+  "52": {
+    "name": "KNEE_MAX",
+    "n": "TAGC"
+  },
+  "53": {
+    "name": "ARM_TAG",
+    "n": "GTAC"
+  },
+  "54": {
+    "name": "ARM_TYPE",
+    "n": "CGAT"
+  },
+  "55": {
+    "name": "ARM_LENGTH",
+    "n": "ATCG"
+  },
+  "56": {
+    "name": "ARM_STRETCH",
+    "n": "GACT"
+  },
+  "57": {
+    "name": "ARM_STRETCH2",
+    "n": "TCAG"
+  },
+  "58": {
+    "name": "ARM_STRENGTH",
+    "n": "ATCG"
+  },
+  "59": {
+    "name": "ARM_HAS_HAND",
+    "n": "CGTA"
+  },
+  "60": {
+    "name": "ARM_JOINT_TYPE",
+    "n": "GCTA"
+  },
+  "61": {
+    "name": "ARM_FLEXIBILITY",
+    "n": "ACTG"
+  },
+  "62": {
+    "name": "ARM_FLEX_BIAS",
+    "n": "GCTA"
+  },
+  "63": {
+    "name": "ARM_FORWARD",
+    "n": "CTGA"
+  },
+  "64": {
+    "name": "ARM_SKEW",
+    "n": "TCGA"
+  },
+  "65": {
+    "name": "ARM_NODE_SCALE",
+    "n": "CTGA"
+  },
+  "66": {
+    "name": "HAS_ELBOW",
+    "n": "CATG"
+  },
+  "67": {
+    "name": "ELBOW_RANGE",
+    "n": "CTAG"
+  },
+  "68": {
+    "name": "UPARM_TAG",
+    "n": "ATCG"
+  },
+  "69": {
+    "name": "UPARM_Y",
+    "n": "CTAG"
+  },
+  "70": {
+    "name": "UPARM_ANGLE",
+    "n": "GCAT"
+  },
+  "71": {
+    "name": "UPARM_GOOFY",
+    "n": "ACGT"
+  },
+  "72": {
+    "name": "NECK_TAG",
+    "n": "AGTC"
+  },
+  "73": {
+    "name": "NECK_TYPE",
+    "n": "TACG"
+  },
+  "74": {
+    "name": "NECK_LENGTH",
+    "n": "ATGC"
+  },
+  "75": {
+    "name": "NECK_GIRAFFE",
+    "n": "TCAG"
+  },
+  "76": {
+    "name": "NECK_THICKNESS",
+    "n": "CGTA"
+  },
+  "77": {
+    "name": "NECK_ANGLE",
+    "n": "TACG"
+  },
+  "78": {
+    "name": "NECK_COCK",
+    "n": "CGAT"
+  },
+  "79": {
+    "name": "NECK_JOINT_TYPE",
+    "n": "TGCA"
+  },
+  "80": {
+    "name": "NECK_FLEXIBILITY",
+    "n": "GTCA"
+  },
+  "81": {
+    "name": "NECK_FLEX_BIAS",
+    "n": "GATC"
+  },
+  "82": {
+    "name": "NECK_SLOUCH",
+    "n": "CTGA"
+  },
+  "83": {
+    "name": "NECK_ONTOP",
+    "n": "GTCA"
+  },
+  "84": {
+    "name": "NECK_STIFF",
+    "n": "CATG"
+  },
+  "85": {
+    "name": "NECK_SPEED",
+    "n": "AGCT"
+  },
+  "86": {
+    "name": "HAS_FOOT",
+    "n": "TACG"
+  },
+  "87": {
+    "name": "FOOT_SIZE",
+    "n": "GTAC"
+  },
+  "88": {
+    "name": "FOOT_CLOWN",
+    "n": "GTAC"
+  },
+  "89": {
+    "name": "FOOT_THICKNESS",
+    "n": "TCAG"
+  },
+  "90": {
+    "name": "FOOT_TOE",
+    "n": "AGCT"
+  },
+  "91": {
+    "name": "FOOT_IS_CIRCLE",
+    "n": "GACT"
+  },
+  "92": {
+    "name": "FOOT_BACKWARDS",
+    "n": "AGCT"
+  },
+  "93": {
+    "name": "HAS_HAND",
+    "n": "CGTA"
+  },
+  "94": {
+    "name": "HAND_WIDTH",
+    "n": "GACT"
+  },
+  "95": {
+    "name": "HAND_LENGTH",
+    "n": "CGAT"
+  },
+  "96": {
+    "name": "HAND_FINGER",
+    "n": "CTGA"
+  },
+  "97": {
+    "name": "HEAD_SIZE",
+    "n": "GCTA"
+  },
+  "98": {
+    "name": "HEAD_THICK_SKULL",
+    "n": "TGAC"
+  },
+  "99": {
+    "name": "HEAD_X_GROWTH",
+    "n": "ACGT"
+  },
+  "100": {
+    "name": "HEAD_Y_GROWTH",
+    "n": "TACG"
+  },
+  "101": {
+    "name": "HEAD_ASPECT",
+    "n": "TCAG"
+  },
+  "102": {
+    "name": "HEAD_SQUARE",
+    "n": "CTAG"
+  },
+  "103": {
+    "name": "HEAD_HAS_BACK",
+    "n": "GTCA"
+  },
+  "104": {
+    "name": "HEAD_GIANT",
+    "n": "CTAG"
+  },
+  "105": {
+    "name": "HEAD_SHRUNK",
+    "n": "CATG"
+  },
+  "106": {
+    "name": "HEAD_JOINTED",
+    "n": "CATG"
+  },
+  "107": {
+    "name": "HEAD_CHIMERA",
+    "n": "ATCG"
+  },
+  "108": {
+    "name": "EYE_STYLE",
+    "n": "CATG"
+  },
+  "109": {
+    "name": "BUGEYE",
+    "n": "GACT"
+  },
+  "110": {
+    "name": "EYEBOX_X",
+    "n": "AGCT"
+  },
+  "111": {
+    "name": "EYEBOX_Y",
+    "n": "CGTA"
+  },
+  "112": {
+    "name": "EYEBOX_SIZE",
+    "n": "ATGC"
+  },
+  "113": {
+    "name": "EYE_SIZE",
+    "n": "ACTG"
+  },
+  "114": {
+    "name": "PUPIL_SIZE",
+    "n": "CAGT"
+  },
+  "115": {
+    "name": "HAS_PUPIL",
+    "n": "ATCG"
+  },
+  "116": {
+    "name": "BROW_SIZE",
+    "n": "GATC"
+  },
+  "117": {
+    "name": "BROW_SLANT",
+    "n": "GTAC"
+  },
+  "118": {
+    "name": "EAR_STYLE",
+    "n": "TGCA"
+  },
+  "119": {
+    "name": "EAR_SHAPE",
+    "n": "CGAT"
+  },
+  "120": {
+    "name": "EAR_FLOP",
+    "n": "CTAG"
+  },
+  "121": {
+    "name": "EAR_X",
+    "n": "TAGC"
+  },
+  "122": {
+    "name": "EAR_SIZE",
+    "n": "TCGA"
+  },
+  "123": {
+    "name": "EAR_ASPECT",
+    "n": "ATCG"
+  },
+  "124": {
+    "name": "EAR_SLANT",
+    "n": "GACT"
+  },
+  "125": {
+    "name": "EAR_INTERIOR",
+    "n": "TAGC"
+  },
+  "126": {
+    "name": "TEETH_SHAPE",
+    "n": "TCGA"
+  },
+  "127": {
+    "name": "HAS_MOUTH",
+    "n": "TCGA"
+  },
+  "128": {
+    "name": "MOUTH_Y",
+    "n": "CGAT"
+  },
+  "129": {
+    "name": "MOUTH_SIZE",
+    "n": "CTGA"
+  },
+  "130": {
+    "name": "JAW",
+    "n": "CAGT"
+  },
+  "131": {
+    "name": "TEETH_UPPER",
+    "n": "GATC"
+  },
+  "132": {
+    "name": "TEETH_UPPER2",
+    "n": "CATG"
+  },
+  "133": {
+    "name": "TONGUE",
+    "n": "CGAT"
+  },
+  "134": {
+    "name": "TONGUE_SEGS",
+    "n": "ACGT"
+  },
+  "135": {
+    "name": "NOSE_STYLE",
+    "n": "GTCA"
+  },
+  "136": {
+    "name": "NOSE_INNY",
+    "n": "GCTA"
+  },
+  "137": {
+    "name": "NOSE_Y",
+    "n": "CTAG"
+  },
+  "138": {
+    "name": "NOSE_SIZE",
+    "n": "AGCT"
+  },
+  "139": {
+    "name": "NOSE_INTERIOR",
+    "n": "ATGC"
+  },
+  "140": {
+    "name": "HAS_ANTLERS",
+    "n": "CTGA"
+  },
+  "141": {
+    "name": "ANTLER_X",
+    "n": "TGAC"
+  },
+  "142": {
+    "name": "ANTLER_W",
+    "n": "TACG"
+  },
+  "143": {
+    "name": "ANTLER_H",
+    "n": "TACG"
+  },
+  "144": {
+    "name": "ANTLER_TAPER",
+    "n": "CGAT"
+  },
+  "145": {
+    "name": "ANTLER_POM",
+    "n": "CTGA"
+  },
+  "146": {
+    "name": "ANTLER_COLOR",
+    "n": "ACGT"
+  },
+  "147": {
+    "name": "POM_COLOR",
+    "n": "GCTA"
+  },
+  "148": {
+    "name": "POM_USECOLOR",
+    "n": "GTAC"
+  },
+  "149": {
+    "name": "ANTLER_REC",
+    "n": "TAGC"
+  },
+  "150": {
+    "name": "ANTLER_REC2",
+    "n": "CTGA"
+  },
+  "151": {
+    "name": "ANTLER_FLIP",
+    "n": "TACG"
+  },
+  "152": {
+    "name": "ANTLER_MOD",
+    "n": "TCAG"
+  },
+  "153": {
+    "name": "ANTLER_SCALEH",
+    "n": "GCTA"
+  },
+  "154": {
+    "name": "ANTLER_SCALEW",
+    "n": "TCGA"
+  },
+  "155": {
+    "name": "ANTLER_ANGLE",
+    "n": "GATC"
+  },
+  "156": {
+    "name": "ANTLER_ANGLE2",
+    "n": "ACTG"
+  },
+  "157": {
+    "name": "ANTLER_ANGLE_RAND",
+    "n": "GTAC"
+  },
+  "158": {
+    "name": "ANTLER_T1",
+    "n": "CGAT"
+  },
+  "159": {
+    "name": "ANTLER_T2",
+    "n": "ATGC"
+  },
+  "160": {
+    "name": "HAT_EXISTS",
+    "n": "ACTG"
+  },
+  "161": {
+    "name": "HAT_SIZE",
+    "n": "CATG"
+  },
+  "162": {
+    "name": "HAT_RAKE",
+    "n": "AGTC"
+  },
+  "163": {
+    "name": "HAT_ASPECT",
+    "n": "ATGC"
+  },
+  "164": {
+    "name": "HAT_TAPER",
+    "n": "TGCA"
+  },
+  "165": {
+    "name": "HAT_POM",
+    "n": "TGAC"
+  },
+  "166": {
+    "name": "HAT_POM_IS_LID",
+    "n": "TAGC"
+  },
+  "167": {
+    "name": "HAT_CLONE",
+    "n": "CGTA"
+  },
+  "168": {
+    "name": "HAT_BACK_SCALE",
+    "n": "TAGC"
+  },
+  "169": {
+    "name": "HAT_FRONT_SCALE",
+    "n": "CGTA"
+  },
+  "170": {
+    "name": "HAT_BACK_ANGLE",
+    "n": "ACGT"
+  },
+  "171": {
+    "name": "HAT_FRONT_ANGLE",
+    "n": "CGAT"
+  },
+  "172": {
+    "name": "HAT_ANGLE_RAND",
+    "n": "TCGA"
+  },
+  "173": {
+    "name": "HAT_FLIP",
+    "n": "TACG"
+  },
+  "174": {
+    "name": "HAT_T",
+    "n": "GTAC"
+  },
+  "175": {
+    "name": "BASE_BROWN",
+    "n": "ATCG"
+  },
+  "176": {
+    "name": "BASE_BLACK",
+    "n": "TGCA"
+  },
+  "177": {
+    "name": "BASE_RED",
+    "n": "GTCA"
+  },
+  "178": {
+    "name": "BASE_GREEN",
+    "n": "TCAG"
+  },
+  "179": {
+    "name": "GREEN_KNOCKOUT",
+    "n": "CAGT"
+  },
+  "180": {
+    "name": "BASE_CREAM",
+    "n": "AGCT"
+  },
+  "181": {
+    "name": "ALT_BLUE",
+    "n": "TAGC"
+  },
+  "182": {
+    "name": "SPOT_YELLOW",
+    "n": "TGCA"
+  },
+  "183": {
+    "name": "SKIN_HUE",
+    "n": "CGAT"
+  },
+  "184": {
+    "name": "SKIN_HUE2",
+    "n": "TAGC"
+  },
+  "185": {
+    "name": "SWAP_BASE_SPOT",
+    "n": "ATCG"
+  },
+  "186": {
+    "name": "SWAP_ALT_SPOT",
+    "n": "TGCA"
+  },
+  "187": {
+    "name": "WHITE",
+    "n": "TAGC"
+  },
+  "188": {
+    "name": "WHITE_IS_LETHAL",
+    "n": "AGCT"
+  },
+  "189": {
+    "name": "EYE_HUE",
+    "n": "GCTA"
+  },
+  "190": {
+    "name": "NOSE_HUE",
+    "n": "GACT"
+  },
+  "191": {
+    "name": "HOOF_COLOR",
+    "n": "CTAG"
+  },
+  "192": {
+    "name": "AGOUTI",
+    "n": "ATGC"
+  },
+  "193": {
+    "name": "BELLY_ALT",
+    "n": "TACG"
+  },
+  "194": {
+    "name": "SKIN_HEAD",
+    "n": "GCAT"
+  },
+  "195": {
+    "name": "SKIN_HANDS",
+    "n": "GCAT"
+  },
+  "196": {
+    "name": "FOOT_IS_HOOF",
+    "n": "GACT"
+  },
+  "197": {
+    "name": "RACCOON_EYE",
+    "n": "GCAT"
+  },
+  "198": {
+    "name": "EAR_COMP",
+    "n": "GTCA"
+  },
+  "199": {
+    "name": "TAIL_ALT",
+    "n": "CGTA"
+  },
+  "200": {
+    "name": "PAT_SPLIT",
+    "n": "CGAT"
+  },
+  "201": {
+    "name": "PAT_BELLY",
+    "n": "ACTG"
+  },
+  "202": {
+    "name": "PAT_STRIPE",
+    "n": "CTAG"
+  },
+  "203": {
+    "name": "PAT_SPOT",
+    "n": "ACGT"
+  },
+  "204": {
+    "name": "PAT_PERLIN",
+    "n": "TCAG"
+  },
+  "205": {
+    "name": "PAT_PERLIN2",
+    "n": "GTCA"
+  },
+  "206": {
+    "name": "PAT_PERLIN_SIZE",
+    "n": "GACT"
+  },
+  "207": {
+    "name": "RAMPAGE",
+    "n": "GATC"
+  },
+  "208": {
+    "name": "SPINAL_LOCO",
+    "n": "CTGA"
+  },
+  "209": {
+    "name": "BRAIN_SPASTIC",
+    "n": "TGAC"
+  },
+  "210": {
+    "name": "HIGH_INTELLECT",
+    "n": "CTAG"
+  },
+  "211": {
+    "name": "OMNIVORE",
+    "n": "CATG"
+  },
+  "212": {
+    "name": "LITTER_SIZE",
+    "n": "CTAG"
+  },
+  "213": {
+    "name": "OLD_AGE",
+    "n": "TACG"
+  },
+  "214": {
+    "name": "LIMP",
+    "n": "AGTC"
+  },
+  "215": {
+    "name": "NARCOLEPSY",
+    "n": "TCGA"
+  },
+  "216": {
+    "name": "FLU_IMMUNITY",
+    "n": "CATG"
+  },
+  "217": {
+    "name": "TAIL_WAG",
+    "n": "CTGA"
+  },
+  "218": {
+    "name": "STIFF_JOINTS",
+    "n": "CGAT"
+  },
+  "219": {
+    "name": "L_LEG_SIGNAL",
+    "n": "GTCA"
+  },
+  "220": {
+    "name": "L_LEG_FTOB_REACT",
+    "n": "TCAG"
+  },
+  "221": {
+    "name": "L_LEG_FTOB_EVENT",
+    "n": "GATC"
+  },
+  "222": {
+    "name": "L_LEG_BTOF_REACT",
+    "n": "ACGT"
+  },
+  "223": {
+    "name": "L_LEG_BTOF_EVENT",
+    "n": "TGAC"
+  },
+  "224": {
+    "name": "L_ARM_SIGNAL",
+    "n": "CGAT"
+  },
+  "225": {
+    "name": "L_ARM_FTOB_REACT",
+    "n": "ACGT"
+  },
+  "226": {
+    "name": "L_ARM_FTOB_EVENT",
+    "n": "TGAC"
+  },
+  "227": {
+    "name": "L_ARM_BTOF_REACT",
+    "n": "GATC"
+  },
+  "228": {
+    "name": "L_ARM_BTOF_EVENT",
+    "n": "CATG"
+  },
+  "229": {
+    "name": "L_TAIL_SIGNAL",
+    "n": "ACTG"
+  },
+  "230": {
+    "name": "L_TAIL_FTOB_REACT",
+    "n": "CGAT"
+  },
+  "231": {
+    "name": "L_TAIL_FTOB_EVENT",
+    "n": "TCGA"
+  },
+  "232": {
+    "name": "L_TAIL_BTOF_REACT",
+    "n": "GTCA"
+  },
+  "233": {
+    "name": "L_TAIL_BTOF_EVENT",
+    "n": "ATCG"
+  },
+  "234": {
+    "name": "L_NECK_SIGNAL",
+    "n": "TGAC"
+  },
+  "235": {
+    "name": "L_NECK_FTOB_REACT",
+    "n": "GTAC"
+  },
+  "236": {
+    "name": "L_NECK_FTOB_EVENT",
+    "n": "CATG"
+  },
+  "237": {
+    "name": "L_NECK_BTOF_REACT",
+    "n": "TCAG"
+  },
+  "238": {
+    "name": "L_NECK_BTOF_EVENT",
+    "n": "TCAG"
+  },
+  "239": {
+    "name": "LOCO_SYNC",
+    "n": "CTGA"
+  }
+}		
---
+++ headers.js
@@ -0,0 +1,68 @@
+// ====== BioHacker Tools Navigation Links ======
+// Edit this file to add or remove tool links in the header dropdown.
+// Each link needs: name, url, icon (emoji or text)
+
+const HEADER_LINKS = [
+    {
+        name: 'CRISPR 2.0',
+        url: 'https://zyonixgaming.github.io/crispr/',
+        icon: '🧬'
+    },
+    {
+        name: 'SIMPR Horsey Genome Editor',
+        url: 'https://zyonixgaming.github.io/crispr/simpr.html',
+        icon: '🧬'
+    },    {
+        name: 'DNA Shortener',
+        url: 'https://zyonixgaming.github.io/dna/',
+        icon: '🔗'
+    },
+    {
+        name: 'BioHacker Optimizer',
+        url: 'https://zyonixgaming.github.io/crispr/biohack.html',
+        icon: '🧪'
+    },
+    {
+        name: 'Lost Horsey Finder',
+        url: 'https://zyonixgaming.github.io/dna/lost.html',
+        icon: '🔎'
+    }
+	
+	
+];
+
+// Populate the dropdown menu
+(function populateHeaderLinks() {
+    const menu = document.getElementById('dropdownMenu');
+    if (!menu) {
+        console.warn('header-links.js: dropdownMenu element not found');
+        return;
+    }
+    let html = '';
+    HEADER_LINKS.forEach(link => {
+        const isCurrent = link.url === window.location.pathname || link.url === window.location.href;
+        html += `<a href="${link.url}" target="${isCurrent ? '_blank' : '_blank'}" rel="noopener">
+            <span class="tool-icon">${link.icon}</span> ${link.name}
+        </a>`;
+    });
+    menu.innerHTML = html;
+})();
+
+
+        document.getElementById('toolsBtn').addEventListener('click', function(e) {
+            e.stopPropagation();
+            const menu = document.getElementById('dropdownMenu');
+            const btn = this;
+            menu.classList.toggle('show');
+            btn.classList.toggle('open');
+        });
+
+        document.addEventListener('click', function(e) {
+            const dropdown = document.getElementById('toolsDropdown');
+            const menu = document.getElementById('dropdownMenu');
+            const btn = document.getElementById('toolsBtn');
+            if (!dropdown.contains(e.target)) {
+                menu.classList.remove('show');
+                btn.classList.remove('open');
+            }
+        });
